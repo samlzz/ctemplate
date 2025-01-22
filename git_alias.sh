a8e3e00509aca228b9b283966719a91da205b05f
um4ss <sliziard@icloud.com>
2025-01-22 12:02:13 +0100

    add alias gd for git diff and gp for git push

--- git_alias.sh
+++ git_alias.sh
@@ -1,8 +1,9 @@
 alias gs="git status"
 alias gl="git log | bat"
+alias gd="git diff | bat"
 alias gcl="git clone"
 alias ga="git add"
 alias gaa="git add ."
 alias gcm="git commit -m"
 alias gca="git commit --amend"
-
+alias gp="git push"
