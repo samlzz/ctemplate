6ffc6eae17645a546e309aabd8880caac180c91d
um4ss <sliziard@icloud.com>
2025-05-04 18:25:45 +0200

    fix: define var as local inside of functions

--- utils.sh
+++ utils.sh
@@ -13,34 +13,33 @@ alias py="python3"
 
 exinit() {
 	if [ -z "$1" ]; then
-        echo "Error: No target dir provided."
-        exit 1
+		echo "Error: No target dir provided."
+		exit 1
 	fi
 	local target_dir="$1"
 	local curr_dir="$PWD"
 
-	while [[ "$curr_dir" != "/" ]];
-	do
-		if [[ -d "$curr_dir/.exemple" ]];
-		then
+	while [[ "$curr_dir" != "/" ]]; do
+		if [[ -d "$curr_dir/.exemple" ]]; then
 			mkdir -p "$target_dir"
-            cp -r "$curr_dir/.exemple/." "$target_dir/"
+			cp -r "$curr_dir/.exemple/." "$target_dir/"
 			echo "$target_dir was correctly init from $curr_dir"
 			return
 		fi
 		curr_dir="$(dirname "$curr_dir")"
 	done
 
-	echo "Error: No `.exemple` folder founded."
+	echo "Error: No $(.exemple) folder founded."
 	exit 1
 }
 
 #? for get the prototype of a function in man
 manproto() {
 	if [ -z "$1" ]; then
-        echo "Error: No function name provided."
-        exit 1
+		echo "Error: No function name provided."
+		exit 1
 	fi
+	local result section
 	result=$(man "$1" | grep "$1(" -A 1 | head -5)
 	section=$(echo "$result" | grep -oP "$1\(\K[0-9]+")
 	if [[ $section =~ ^[0-9]+$ ]]; then
@@ -51,23 +50,23 @@ manproto() {
 }
 
 ccreate() {
-	filename=$1
-	h=$2
-	c=$3
-	def_name=$(echo "$filename" | tr '[:lower:]' '[:upper:]')_H
-	
-	touch "$filename.$h" 
+	local filename=$1
+	local h=$2
+	local c=$3
+	local def_name=$(echo "$filename" | tr '[:lower:]' '[:upper:]')_H
 
-	echo "#ifndef $def_name" > "$filename.$h"
-	echo "# define $def_name" >> "$filename.$h"
-	echo "" >> "$filename.$h"
-	echo "#endif" >> "$filename.$h"
+	touch "$filename.$h"
+
+	echo "#ifndef $def_name" >"$filename.$h"
+	echo "# define $def_name" >>"$filename.$h"
+	echo "" >>"$filename.$h"
+	echo "#endif" >>"$filename.$h"
 
 	echo -n "Do you want to create the source file $filename.$c? (Y/n):"
 	read -r create_source
-    create_source=${create_source:-y}
+	create_source=${create_source:-y}
 	if [[ "$create_source" =~ ^[yY]$ ]]; then
-		echo "#include \"$filename.$h\"" > "$filename.$c"
+		echo "#include \"$filename.$h\"" >"$filename.$c"
 		echo "Files(.$h, .$c) were created successfully"
 	else
 		echo "File(.$h) was successfully created"
@@ -77,8 +76,8 @@ ccreate() {
 #? create an .hpp file with is assosiated .cpp
 hppcreate() {
 	if [ -z "$1" ]; then
-        echo "Error: No filename provided."
-        exit 1
+		echo "Error: No filename provided."
+		exit 1
 	fi
 	ccreate "$1" hpp cpp
 }
@@ -86,16 +85,16 @@ hppcreate() {
 #? create an .h file with is assosiated .c
 hcreate() {
 	if [ -z "$1" ]; then
-        echo "Error: No filename provided."
-        exit 1
+		echo "Error: No filename provided."
+		exit 1
 	fi
 	ccreate "$1" h c
 }
 
 #? init current directory to be a project directory
 ftinit() {
-	tmp_dir="$HOME/ctemplate"  #? exemples files dir
-	curr_dir=$(pwd)
+	local tmp_dir="$HOME/ctemplate" #? exemples files dir
+	local curr_dir=$(pwd)
 
 	#? tmp_dir doesn't exist ? reitriving from github
 	if [ ! -d "$tmp_dir" ]; then
@@ -106,7 +105,7 @@ ftinit() {
 			git clone git@github.com:samlzz/template.git "$tmp_dir" || {
 				echo "Error: Failed to clone repository."
 				exit 1
-            }
+			}
 		else
 			exit 1
 		fi
@@ -132,9 +131,9 @@ ftinit() {
 	mkdir src
 }
 
-gupdate()
-{
-	curr_dir=$(pwd)
+gupdate() {
+	local curr_dir=$(pwd)
+	local default_repos repos_to_update updated_repo
 	default_repos=("$HOME/ctemplate")
 	if [ "$#" -eq 0 ]; then
 		echo "[INFO] no dir given, use default directory."
@@ -144,80 +143,81 @@ gupdate()
 	fi
 	updated_repos=()
 
+	local repo_path
 	for repo_path in "${repos_to_update[@]}"; do
 		if [[ "$repo_path" != /* ]]; then
-		    repo_path="$curr_dir/$repo_path"
+			repo_path="$curr_dir/$repo_path"
 		fi
 
 		if [ ! -d "$repo_path" ]; then
-		    echo "[ERREUR] '$repo_path' doesn't exist."
-		    continue
+			echo "[ERREUR] '$repo_path' doesn't exist."
+			continue
 		fi
 
 		cd "$repo_path" || {
-		    echo "[ERREUR] fail to navige to $repo_path."
-		    continue
+			echo "[ERREUR] fail to navige to $repo_path."
+			continue
 		}
 
 		if [ ! -d ".git" ]; then
-		    echo "[ERREUR] $repo_path wasn't a Git repository."
-		    continue
+			echo "[ERREUR] $repo_path wasn't a Git repository."
+			continue
 		fi
 
 		echo "[INFO] Mise à jour du dépôt dans $repo_path..."
 		git fetch --all
+		local pull_output
 		pull_output=$(git pull 2>&1)
 
 		if [ $? -eq 0 ] && [[ "$pull_output" != *"Already up to date."* ]]; then
-		    updated_repos+=("$repo_path")
+			updated_repos+=("$repo_path")
 		fi
 	done
 
-	cd "$curr_dir" > /dev/null
-	
+	cd "$curr_dir" >/dev/null
+
 	# Affiche les résultats
 	echo -e "\n[RÉSUMÉ] Dossiers mis à jour :"
 	if [ ${#updated_repos[@]} -eq 0 ]; then
 		echo "Aucun dépôt n'a été mis à jour."
 	else
 		for updated_repo in "${updated_repos[@]}"; do
-	    		echo "- $updated_repo"
+			echo "- $updated_repo"
 		done
 	fi
 }
 
 managedns() {
-    case "$1" in
-        off)
-            echo "[INFO] Désactivation de la gestion DNS par NetworkManager..."
-            sudo mkdir -p /etc/NetworkManager/conf.d
-            echo -e "[main]\ndns=none" | sudo tee /etc/NetworkManager/conf.d/dns.conf > /dev/null
-	    cat /etc/myresolv.conf | sudo tee /etc/resolv.conf > /dev/null
-            sudo chattr +i /etc/resolv.conf
-            sudo systemctl restart NetworkManager
-            echo "[OK] Gestion DNS désactivée. Utilisation de Cloudflare et Google."
-            ;;
+	case "$1" in
+	off)
+		echo "[INFO] Désactivation de la gestion DNS par NetworkManager..."
+		sudo mkdir -p /etc/NetworkManager/conf.d
+		echo -e "[main]\ndns=none" | sudo tee /etc/NetworkManager/conf.d/dns.conf >/dev/null
+		cat /etc/myresolv.conf | sudo tee /etc/resolv.conf >/dev/null
+		sudo chattr +i /etc/resolv.conf
+		sudo systemctl restart NetworkManager
+		echo "[OK] Gestion DNS désactivée. Utilisation de Cloudflare et Google."
+		;;
 
-        on)
-            echo "[INFO] Réactivation de la gestion DNS par NetworkManager..."
-            sudo rm -f /etc/NetworkManager/conf.d/dns.conf
-            sudo chattr -i /etc/resolv.conf
-            sudo systemctl restart NetworkManager
-            echo "[OK] Gestion DNS réactivée par NetworkManager."
-            ;;
+	on)
+		echo "[INFO] Réactivation de la gestion DNS par NetworkManager..."
+		sudo rm -f /etc/NetworkManager/conf.d/dns.conf
+		sudo chattr -i /etc/resolv.conf
+		sudo systemctl restart NetworkManager
+		echo "[OK] Gestion DNS réactivée par NetworkManager."
+		;;
 
-        state)
-            if [[ -f /etc/NetworkManager/conf.d/dns.conf ]]; then
-                echo "[STATUS] Gestion DNS : ❌ Désactivée (NetworkManager ne gère pas le DNS)"
-            else
-                echo "[STATUS] Gestion DNS : ✅ Activée (NetworkManager gère le DNS)"
-            fi
-            ;;
+	state)
+		if [[ -f /etc/NetworkManager/conf.d/dns.conf ]]; then
+			echo "[STATUS] Gestion DNS : ❌ Désactivée (NetworkManager ne gère pas le DNS)"
+		else
+			echo "[STATUS] Gestion DNS : ✅ Activée (NetworkManager gère le DNS)"
+		fi
+		;;
 
-        *)
-            echo "Usage: manage_dns on|off|state"
-            return 1
-            ;;
-    esac
+	*)
+		echo "Usage: manage_dns on|off|state"
+		return 1
+		;;
+	esac
 }
-
